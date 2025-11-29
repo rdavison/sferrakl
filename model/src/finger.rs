216236a70dd6ef059f96b9bfea717ed98832c6d8
Richard Davison <richarddavison@macbookair.myfiosgateway.com>
2025-11-29 00:26:35 -0500

    add finger module

---
+++ model/src/finger.rs
@@ -0,0 +1,8 @@
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+pub enum Finger {
+    P, // Pinky
+    R, // Ring
+    M, // Middle
+    I, // Index
+    T, // Thumb
+}
--- model/src/lib.rs
+++ model/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod bigram;
+pub mod finger;
 pub mod key;
 
 #[cfg(test)]
@@ -6,6 +7,7 @@ mod tests {
 
     use super::key::Src;
     use crate::bigram;
+    use crate::finger::Finger;
     use crate::key::ANSI30;
 
     #[test]
@@ -25,5 +27,14 @@ mod tests {
     fn bigram_init() {
         let bigram_map = bigram::Map::init(Src::Ansi30, &|_, _| 1.0f64);
         assert_eq!(bigram_map.0.len(), ANSI30.len() * ANSI30.len());
+    }
+
+    #[test]
+    fn finger_enum() {
+        assert_eq!(format!("{:?}", Finger::P), "P");
+        assert_eq!(format!("{:?}", Finger::R), "R");
+        assert_eq!(format!("{:?}", Finger::M), "M");
+        assert_eq!(format!("{:?}", Finger::I), "I");
+        assert_eq!(format!("{:?}", Finger::T), "T");
     }
 }
