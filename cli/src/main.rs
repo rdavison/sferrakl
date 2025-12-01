33f712ba5acbc8427e30a6df647240a6aac66004
Richard Davison <richard.neil.davison@gmail.com>
2025-11-30 21:56:49 -0500

    add clap dep

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,24 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Prints "Hello, world!"
+    Hello,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Hello => {
+            println!("Hello, world!");
+        }
+    }
 }
